f98c516a1bd1f49eb741fe0826ec4474d8e2d145
zndy <984761675@qq.com>
2018-05-28 17:19:39 +0800

    修改nginx配置

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '120.55.60.52:22',
+    'hostname'       => 'localhost',
     // 数据库名
     'database'       => 'xhshop',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'dsaixiaer',
+    'password'       => '123456',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
